09232b9a05343e2abadc03565eed74fba18fef63
SAMYTA RAMIZA VICENTE DA SILVA <20221170150002@ifrn.local>
2023-09-28 11:53:41 -0300

    Código

--- Projeto 2/Assets/Scripts/Player.cs
+++ Projeto 2/Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int velocidade = 10;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debog.Log("STAT");
+        TryGetComponent(out rb);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debog.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra trás, 0 nada, 1 pra frente
+
+        Vector3 direcao = new Vector3(h, 0, v);
         
     }
 }
