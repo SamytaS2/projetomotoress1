25f5d46a2fb18922cb300903eb0785bfe56059b3
SAMYTA RAMIZA VICENTE DA SILVA <20221170150002@ifrn.local>
2023-09-28 14:51:28 -0300

    Bolinha andando

--- Projeto 2/Assets/Scripts/Player.cs
+++ Projeto 2/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -9,19 +10,24 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debog.Log("STAT");
+        Debug.Log("STAT");
         TryGetComponent(out rb);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debog.Log("UPDATE");
+        Debug.Log("UPDATE");
         float h = Input.GetAxis("Horizontal"); // -1 esquerda, 0 nada, 1 direita
         float v = Input.GetAxis("Vertical"); // -1 pra trás, 0 nada, 1 pra frente
 
         Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if(transform.position.y <= -10){
+            // jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
         
     }
 }
