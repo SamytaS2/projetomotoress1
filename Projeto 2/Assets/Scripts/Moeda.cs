f672b16d7fa92e29009d44059921c0c868076a9d
SAMYTA RAMIZA VICENTE DA SILVA <20221170150002@ifrn.local>
2023-10-11 15:32:29 -0300

    Moedas

---
+++ Projeto 2/Assets/Scripts/Moeda.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidadeGiro * Time.deltaTime,Space.World);
+        
+    }
+}
