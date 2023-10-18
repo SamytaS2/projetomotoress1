43c22d672e687fd2bbb2f623ff5fbdcaf1376d3a
SAMYTA RAMIZA VICENTE DA SILVA <20221170150002@ifrn.local>
2023-10-18 10:27:01 -0300

    dia 18

--- Projeto 2/Assets/Scripts/GameManager.cs
+++ Projeto 2/Assets/Scripts/GameManager.cs
@@ -7,11 +7,26 @@ public class GameManager : MonoBehaviour
 {
 
     public TextMeshProUGUI hud, msgVitoria;
+    public int restantes;
     
     // Start is called before the first frame update
     void Start()
     {
+        restantes = FindObjectsOfType<Moeda>().Length;
+
+        hud.text = $"Moedas restantes: {restantes}";
+    }
+
+    public void SubtrairMoedas(int valor)
+    {
+        restantes = restantes - valor;
+        hud.text = $"Moedas restantes: {restantes}";
         
+        if (restantes <= 0)
+        {
+            //Ganhou o jogo
+            msgVitoria.text = "Parabéns!!!";
+        }
     }
 
     // Update is called once per frame
--- Projeto 2/Assets/Scripts/Moeda.cs
+++ Projeto 2/Assets/Scripts/Moeda.cs
@@ -17,6 +17,7 @@ public class Moeda : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            FindObjectOfType<GameManager>().SubtrairMoedas(1);
             Destroy(gameObject);
         }
     }
